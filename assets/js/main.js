5debbba6f48015565ddeb551c2876de618527117
null-dimension <4hrishi4@gmail.com>
2021-08-12 20:17:09 +0530

    Change window width and height

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ new WinBox("null-dimension.github.io", {
     mount: document.getElementById("content"),
     x: "center",        
     y: "center",
-    width: "50%",
-    height: "50%"
+    width: "75%",
+    height: "75%"
 });
