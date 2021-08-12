16c180e3e6aa162aa28c2e5ecbfd9937b264f295
null-dimension <4hrishi4@gmail.com>
2021-08-12 19:57:59 +0530

    Added winbox

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+new WinBox("null-dimension.github.io", {
+    class: "modern",
+    mount: document.getElementById("content"),
+    x: "center",        
+    y: "center",
+    width: "50%",
+    height: "50%"
+});
